5c3f4f19be13d706a92ef249275f64d3d8fcbe79
DonizeteVida <donizetevida1998@outlook.com>
2025-01-22 21:13:49 -0300

    Basic input handling

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ use macroquad::prelude::*;
 
 #[macroquad::main("MyGame")]
 async fn main() {
+    let mut x = 10.0;
+    let mut y = 10.0;
     loop {
-        clear_background(RED);
-
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        if is_key_down(KeyCode::A) {
+            x = x - 1.0;
+        }
+        if is_key_down(KeyCode::D) {
+            x = x + 1.0;
+        } 
+        if is_key_down(KeyCode::S) {
+            y = y + 1.0;
+        }
+        if is_key_down(KeyCode::W) {
+            y = y - 1.0;
+        }
+        draw_rectangle(x, y, 1.0, 1.0, RED);
         next_frame().await
     }
 }
